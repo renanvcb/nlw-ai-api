78f3ff2c327bd7f568b357dd485758cd8f1bee52
Renan Borges <renanvcb@gmail.com>
2023-09-13 18:01:22 -0300

    Final Aula 02

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify"
 import { createReadStream } from 'node:fs'
 import { z } from 'zod'
 import { prisma } from "../lib/prisma"
+import { openai } from "../lib/openai"
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post('/videos/:videoId/transcription', async (request) => {
@@ -26,10 +27,26 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
     const videoPath = video.path
     const audioReadStream = createReadStream(videoPath)
 
-    return {
-      videoId,
-      prompt,
-      videoPath
-    }
+    const response = await openai.audio.transcriptions.create({
+      file: audioReadStream,
+      model: 'whisper-1',
+      language: 'pt',
+      response_format: 'json',
+      temperature: 0,
+      prompt
+    })
+
+    const transcription = response.text
+
+    await prisma.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription
+      }
+    })
+
+    return { transcription }
   });
 }
