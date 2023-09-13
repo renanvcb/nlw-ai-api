0107641f6f86b91e598cfa34805ccbb3c985299e
renanvcb <renanvcb@gmail.com>
2023-09-13 13:05:15 -0300

    starting transcription

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
-import { fastify } from "fastify";
+import { fastify } from 'fastify';
+import { getAllPromptsRoute } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
+import { createTranscriptionRoute } from './routes/create-transcription';
 
 const app = fastify();
 
-app.get('/', () => {
-  return 'Hello World'
-});
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({
   port: 3333,
 }).then(() => {
-  console.log('Server running on port 3333');
+  console.log('Server running on port 3333.');
 });
